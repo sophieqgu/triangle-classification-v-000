6280979fcd87c34463bb0806b024bc7905d04024
Sophie Gu <sophieqgu@gmail.com>
2019-03-28 17:38:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
       if @side_1 == @side_2 && @side_2 == @side_3
         :equilateral 
       elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3 
-        :isoceles
+        :isosceles
       else 
         :scalene 
       end 
