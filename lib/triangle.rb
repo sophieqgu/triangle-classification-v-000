78ded3638cd0c52212e2cee56cab18d60703f657
Sophie Gu <sophieqgu@gmail.com>
2019-03-28 17:11:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
-  attr_accessor 
+  attr_accessor :side_1, :side_2, :side_3
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end 
+  
+  def kind 
+    if @side_1 + @side_2 > @side_3 && 
+      :equilateral if @side_1 == @side_2 == @side_3
+    
 end
